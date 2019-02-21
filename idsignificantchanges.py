5578ffa65544020c0f02ee68d0f217eb1a2e0a50
az20304 <zhual@utschools.ca>
2019-02-20 23:54:58 -0500

    capture clicks

--- idsignificantchanges.py
+++ idsignificantchanges.py
@@ -1,6 +1,6 @@
 import json;
-import tkinter as tk;
-#Written by Alex Zhuang zhual@utschools.ca
+import tkinter;
+#written by Alex Zhuang zhual@utschools.ca
 with open('inputmap.json', 'r') as f:
     inputmap = json.load(f)
 with open('inputsurveyphotos.json', 'r') as f:
@@ -8,5 +8,16 @@ with open('inputsurveyphotos.json', 'r') as f:
 
 print (inputmap)
 print (inputsurveyphotos)
-top = tk.Tk()
+top = tkinter.Tk()
+C = tkinter.Canvas(top, bg="blue", height=250, width=300)
+
+coord = 10, 50, 240, 210
+arc = C.create_arc(coord, start=0, extent=150, fill="red")
+
+C.pack()
+def callback(event):
+    print ("clicked at", event.x, event.y)
+
+top.bind("<Button-1>", callback)
+
 top.mainloop()
